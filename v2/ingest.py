f0c34c9deea267f3e453b27a98959ea3115f0c9b
Aramrac <camaramaungu@gmail.com>
2026-04-25 01:44:43 +0300

    Call function

--- v2/ingest.py
+++ v2/ingest.py
@@ -1,4 +1,5 @@
-from ingestion import update_vector_store
+from ingestion import update_vector_store, build_topics_json
 
 if __name__ == "__main__":
     update_vector_store(force_reload=True)
+    build_topics_json()
